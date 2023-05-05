3612ac6d8c19f2efef5d280d3934570e590159d4
juantorresdev <juan.torres.dev@gmail.com>
2023-05-04 23:47:28 -0600

    9 Generated Address Response and fixed getAddres Endpoint

--- PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/AddressController.java
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/AddressController.java
@@ -1,12 +1,21 @@
 package com.pharos.PharosDBmapper.controller;
 
+import com.pharos.PharosDBmapper.dto.AddressDTO;
 import com.pharos.PharosDBmapper.entities.Address;
 import com.pharos.PharosDBmapper.services.AddressService;
 import com.pharos.PharosDBmapper.services.NationalityService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.pharos.PharosDBmapper.wrappers.response.*;
 
 @RestController
 @RequestMapping("/api/v1/address")
@@ -17,9 +26,16 @@ public class AddressController {
     private final AddressService addressService;
     
     @GetMapping("/getAddress")
-    public ResponseEntity<PharosDBMapperResponse> getAddress(@RequestParam("idAddress") int idAddress){
+    public ResponseEntity<PharosDBMapperResponse> getAddress(@RequestParam("addressId") int addressId) {
 
-        Address address = addressService.readAddress(null);
+        log.info("Accessed to /api/v1/address/getAddress endpoint");
+
+        try {
+            AddressResponse addressResponse = addressService.readAddress(addressId);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
         PharosDBMapperResponse pharosDBMapperResponse = new PharosDBMapperResponse();
         ResponseEntity.BodyBuilder bodyBuilder = ResponseEntity.status(HttpStatus.OK);
---
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/wrappers/response/AddressResponse.java
@@ -0,0 +1,13 @@
+package com.pharos.PharosDBmapper.wrappers.response;
+
+public class AddressResponse {
+    
+    private Integer id;
+    private String street;
+    private String city;
+    private String province;
+    private String postcode;
+    private String country;
+    private String status;
+
+}
