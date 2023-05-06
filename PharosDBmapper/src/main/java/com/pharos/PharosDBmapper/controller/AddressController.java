c7160932d04fb30be95b13f2c9d893f5a36aa1e7
juantorresdev <juan.torres.dev@gmail.com>
2023-05-05 23:17:56 -0600

    10 Fixed endpoint /getAddress and added /createAddress

--- PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/AddressController.java
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/AddressController.java
@@ -4,18 +4,26 @@ import com.pharos.PharosDBmapper.dto.AddressDTO;
 import com.pharos.PharosDBmapper.entities.Address;
 import com.pharos.PharosDBmapper.services.AddressService;
 import com.pharos.PharosDBmapper.services.NationalityService;
+import com.pharos.PharosDBmapper.services.impl.AddressServiceImpl;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-import com.pharos.PharosDBmapper.wrappers.response.*;
+import com.pharos.PharosDBmapper.wrappers.response.PharosDBMapperResponse;
+import com.pharos.PharosDBmapper.wrappers.response.AddressResponse;
+import com.pharos.PharosDBmapper.wrappers.request.AddressRequest;
 
 @RestController
 @RequestMapping("/api/v1/address")
@@ -23,23 +31,41 @@ import com.pharos.PharosDBmapper.wrappers.response.*;
 @Slf4j
 public class AddressController {
 
+    @NonNull
     private final AddressService addressService;
     
+    @PostMapping("/createAddress")
+    public ResponseEntity<PharosDBMapperResponse> createAddress(@RequestBody AddressRequest addressRequest){
+
+        log.info("Accessed to /api/v1/address/createAddress endpoint");
+        PharosDBMapperResponse pharosDBMapperResponse = new PharosDBMapperResponse();
+
+        try {
+            AddressResponse addressResponse = addressService.createAddress(addressRequest);
+            pharosDBMapperResponse.setData(addressResponse);
+            
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(pharosDBMapperResponse);
+
+    }
+
     @GetMapping("/getAddress")
     public ResponseEntity<PharosDBMapperResponse> getAddress(@RequestParam("addressId") int addressId) {
 
         log.info("Accessed to /api/v1/address/getAddress endpoint");
+        PharosDBMapperResponse pharosDBMapperResponse = new PharosDBMapperResponse();
+        BodyBuilder bodyBuilder;
 
         try {
             AddressResponse addressResponse = addressService.readAddress(addressId);
+            bodyBuilder = ResponseEntity.status(HttpStatus.OK);
         } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+            bodyBuilder = ResponseEntity.status(HttpStatus.NOT_FOUND);
         }
-
-        PharosDBMapperResponse pharosDBMapperResponse = new PharosDBMapperResponse();
-        ResponseEntity.BodyBuilder bodyBuilder = ResponseEntity.status(HttpStatus.OK);
         return bodyBuilder.body(pharosDBMapperResponse);
 
     }
+
 }
--- PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/services/impl/AddressServiceImpl.java
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/services/impl/AddressServiceImpl.java
@@ -5,7 +5,9 @@ import com.pharos.PharosDBmapper.entities.Address;
 import com.pharos.PharosDBmapper.mappers.AddressMapper;
 import com.pharos.PharosDBmapper.repository.AddressRepository;
 import com.pharos.PharosDBmapper.services.AddressService;
+import com.pharos.PharosDBmapper.wrappers.request.AddressRequest;
 import com.pharos.PharosDBmapper.wrappers.response.AddressResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,16 +22,13 @@ public class AddressServiceImpl implements AddressService {
     private final AddressRepository addressRepository;
     private final AddressMapper addressMapper;
 
-
     @Override
-    public AddressResponse createAddress(AddressDTO addressDTO) throws Exception {
+    public AddressResponse createAddress(AddressRequest addressRequest) throws Exception {
 
         log.info("Accessed to createAddress service");
 
-        Address address = addressMapper.dtoToAddress(addressDTO);
+        Address address = addressMapper.addressRequestToAddress(addressRequest);
         addressRepository.save(address);
-
-        AddressResponse addressResponse = addressMapper.addressToResponse(address);
 
         return addressMapper.addressToResponse(address);
     }
@@ -45,7 +44,7 @@ public class AddressServiceImpl implements AddressService {
     //TODO - Read Address Method implementing JPA Criteria
 
     @Override
-    public List<AddressResponse> readAddressList(AddressDTO addressDTO) throws Exception {
+    public List<AddressResponse> readAddressList(AddressRequest addressRequest) throws Exception {
 
         List<Address> addressList = addressRepository.findAll();
 
@@ -54,12 +53,12 @@ public class AddressServiceImpl implements AddressService {
     }
 
     @Override
-    public AddressResponse updateAddress(AddressDTO addressDTO) throws Exception {
+    public AddressResponse updateAddress(AddressRequest addressRequest) throws Exception {
         return null;
     }
 
     @Override
-    public AddressResponse deleteAddress(AddressDTO addressDTO) throws Exception {
+    public AddressResponse deleteAddress(AddressRequest addressRequest) throws Exception {
         return null;
     }
 }
