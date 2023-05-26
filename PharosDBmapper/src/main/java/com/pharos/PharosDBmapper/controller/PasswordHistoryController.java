9588946c359fd2f90a6e602a563e1fced0e604d5
Juan Torres <57916724+juantorresdev@users.noreply.github.com>
2023-05-26 05:00:15 +0000

    32 Created new Password enpoint

--- PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/PasswordHistoryController.java
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/PasswordHistoryController.java
@@ -10,8 +10,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/password")
 @Slf4j
-//@RequiredArgsConstructor
+@RequiredArgsConstructor
 public class PasswordHistoryController {
 
-    //public final PasswordHistoryService passwordHistoryService;
+    public final PasswordHistoryService passwordHistoryService;
+
+    @PostMapping("/createPassword")
+    public ResponseEntity<PharosDBMapperResponse> createPassword(@RequestBody PasswordRequest passwordRequest){
+
+        log.info("Accessed to /api/v1/password/createPassword endpoint");
+        PharosDBMapperResponse pharosDBMapperResponse = new PharosDBMapperResponse();
+
+        try {
+            AddressResponse addressResponse = passwordServicee.createAddress(addressRequest);
+            pharosDBMapperResponse.setData(addressResponse);
+            
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(pharosDBMapperResponse);
+
+    }
 }
