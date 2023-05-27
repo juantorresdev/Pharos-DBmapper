e4e4bac96e2186a11c91f72882b0e73e6e5772f5
juantorresdev <juan.torres.dev@gmail.com>
2023-05-26 23:47:00 -0600

    33 Generated Password methods on History Service interface

--- PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/PasswordHistoryController.java
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/controller/PasswordHistoryController.java
@@ -1,9 +1,17 @@
 package com.pharos.PharosDBmapper.controller;
 
 import com.pharos.PharosDBmapper.services.PasswordHistoryService;
-import lombok.AllArgsConstructor;
+import com.pharos.PharosDBmapper.wrappers.request.PasswordHistoryRequest;
+import com.pharos.PharosDBmapper.wrappers.response.PasswordHistoryResponse;
+import com.pharos.PharosDBmapper.wrappers.response.PharosDBMapperResponse;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +24,14 @@ public class PasswordHistoryController {
     public final PasswordHistoryService passwordHistoryService;
 
     @PostMapping("/createPassword")
-    public ResponseEntity<PharosDBMapperResponse> createPassword(@RequestBody PasswordRequest passwordRequest){
+    public ResponseEntity<PharosDBMapperResponse> createPassword(@RequestBody PasswordHistoryRequest passwordRequest){
 
         log.info("Accessed to /api/v1/password/createPassword endpoint");
         PharosDBMapperResponse pharosDBMapperResponse = new PharosDBMapperResponse();
 
         try {
-            AddressResponse addressResponse = passwordServicee.createAddress(addressRequest);
-            pharosDBMapperResponse.setData(addressResponse);
+            PasswordHistoryResponse passwordHistoryResponse = passwordHistoryService.createPassword(passwordRequest);
+            pharosDBMapperResponse.setData(passwordHistoryResponse);
             
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
