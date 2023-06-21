dadd80211ebaeb25cb754af969c601827d48a9d6
juantorresdev <juan.torres.dev@gmail.com>
2023-06-20 22:09:52 -0600

    48 Added SpringFox dependency to fix Swagger configuration

--- PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/config/SwaggerConfig.java
+++ PharosDBmapper/src/main/java/com/pharos/PharosDBmapper/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.pharos.service.configuration;
+package com.pharos.PharosDBmapper.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
